8a2ef1e5c5a99977880d66ea4b0bfe28d25843d1
yujin <ckdbwls11@naver.com>
2016-07-09 16:54:35 +0900

    reviewers test, first commit

---
+++ reviewers/migrations/0003_auto_20160701_2046.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.utils.timezone
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('reviewers', '0002_auto_20160701_2042'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='post',
+            name='published_date',
+            field=models.DateTimeField(default=django.utils.timezone.now),
+        ),
+    ]
